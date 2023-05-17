a19638cdbb7d3bf771f8954a81df491b3bd598b6
huxiangyuan <huxiangyuan@harmonycloud.cn>
2023-05-17 17:21:47 +0800

    Add camera-front-config
    Signed-off-by: huxiangyuan <huxiangyuan@harmonycloud.cn>

--- deploy/agent/install.sh
+++ deploy/agent/install.sh
@@ -4,4 +4,5 @@ kubectl create ns kindling-hxy
 kubectl create serviceaccount kindling-agent -nkindling-hxy
 kubectl apply -f kindling-clusterrolebinding.yml
 kubectl create cm kindlingcfg -n kindling-hxy --from-file=kindling-collector-config.yml
+kubectl apply -f camera-front-configmap.yml
 kubectl apply -f kindling-deploy.yml
--- deploy/agent/uninstall.sh
+++ deploy/agent/uninstall.sh
@@ -2,5 +2,6 @@
 
 kubectl delete -f kindling-deploy.yml
 kubectl delete cm kindlingcfg -n kindling-hxy
+kubectl delete cm camera-front-config -n kindling-hxy
 kubectl delete -f kindling-clusterrolebinding.yml
 kubectl delete serviceaccount kindling-agent -nkindling-hxy
